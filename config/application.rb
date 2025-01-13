0e5370692818a6b873ab5f1799c7f6d8a981c5fa
tairar <2211140044s@ed.fuk.kindai.ac.jp>
2025-01-14 02:25:03 +0900

    楽曲詳細・コメントの表示を調整

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Recomusic
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.encoding = "utf-8"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'links/new'
   get 'top/index'
   get 'top/new'
   get 'tops/index'
